1139b6d292677cd4fe441d7ad272fc0e940df2e6
Casie Siekman <casie.siekman@gmail.com>
2023-01-17 20:05:13 -0600

    routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link, useHistory } from 'react-router-dom';
 import './App.css';
 
 import Animals from '../Animals/Animals.jsx';
@@ -6,10 +7,37 @@ import Plants from '../Plants/Plants.jsx';
 import Home from '../Home/Home.jsx';
 
 function App() {
+  const history = useHistory();
+
   return (
-    <div className="App">
-      <h1>SPAs!</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>SPAs!</h1>
+        <ul>
+          <li>
+            <Link to="/animals">Animals</Link>
+          </li>
+          <li>
+            <Link to="/plants">Plants</Link>
+          </li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+
+
+
+        <Route path="/animals">
+          <Animals />
+        </Route>
+        <Route path="/plants">
+          <Plants />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
